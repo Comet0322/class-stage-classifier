7d6d5ef5391d28213a9b036385d4137ef39a9ba9
Jerry <Comet0322@protonmail.com>
2024-05-31 15:08:32 +0800

    feat: Add metric.py to evaluate model performance

--- metric.py
+++ metric.py
@@ -1,79 +1,73 @@
+from argparse import ArgumentParser
+
 import numpy as np
 import pandas as pd
 from pyannote.core import Annotation, Segment
 from pyannote.metrics.errors.identification import IdentificationErrorAnalysis
 from sklearn.metrics import f1_score
 
-
-def accuracy(references, hypotheses):
-    total_duration = 0
-    correct_duration = 0
-    for reference_df, hypothesis_df in zip(references, hypotheses):
-        start = 0
-        end = reference_df.iloc[-1, 1]
-        reference = Annotation()
-        for i, row in reference_df.iterrows():
-            reference[Segment(row[0], row[1])] = row[2]
-        hypothesis = Annotation()
-        for i, row in hypothesis_df.iterrows():
-            hypothesis[Segment(row[0], row[1])] = row[2]
-        error = IdentificationErrorAnalysis().difference(
-            reference, hypothesis, uem=Segment(start, end)
-        )
-        for segment, _ in error.itertracks():
-            label = error.get_labels(segment)
-            status, true_label, pred_label = next(iter(label))
-            if status == "correct":
-                correct_duration += segment.duration
-            total_duration += segment.duration
-
-    acc = correct_duration / total_duration
-
-    return acc
+import wandb
 
 
-def f1_scores(references, hypotheses):
-    true_labels = []
-    pred_labels = []
-    for reference_df, hypothesis_df in zip(references, hypotheses):
-        start = 0
-        end = reference_df.iloc[-1, 1]
-        reference = Annotation()
-        for i, row in reference_df.iterrows():
-            reference[Segment(row[0], row[1])] = row[2]
-        hypothesis = Annotation()
-        for i, row in hypothesis_df.iterrows():
-            hypothesis[Segment(row[0], row[1])] = row[2]
-        error = IdentificationErrorAnalysis().difference(
-            reference, hypothesis, uem=Segment(start, end)
-        )
+def get_class_maps(num_class):
+    if num_class == 5:
+        class_name_map = {
+            "Introduction_or_Opening": "Opening",
+            "Lecture_or_Presentation": "Lecture",
+            "Break_or_Transition": "Break",
+            "Conclusion_or_Summary": "Conclusion",
+            "Opening": "Opening",
+            "Lecture": "Lecture",
+            "Break": "Break",
+            "Conclusion": "Conclusion",
+            "Others": "Others",
+        }
+        class_dict = {
+            "Opening": 0,
+            "Lecture": 1,
+            "Break": 2,
+            "Conclusion": 3,
+            "Others": 4,
+        }
+        short_class_names = ["Opening", "Lecture", "Break", "Conclusion", "Others"]
+    else:
+        class_name_map = {
+            "Introduction_or_Opening": "Lecture",
+            "Lecture_or_Presentation": "Lecture",
+            "Break_or_Transition": "Others",
+            "Conclusion_or_Summary": "Lecture",
+            "Opening": "Lecture",
+            "Lecture": "Lecture",
+            "Break": "Others",
+            "Conclusion": "Lecture",
+            "Others": "Others",
+        }
+        class_dict = {
+            "Opening": 1,
+            "Lecture": 1,
+            "Break": 0,
+            "Conclusion": 1,
+            "Others": 0,
+        }
+        short_class_names = [
+            "Others",
+            "Lecture",
+        ]
+    return class_name_map, class_dict, short_class_names
 
-        for segment, _ in error.itertracks():
-            label = error.get_labels(segment)
-            status, true_label, pred_label = next(iter(label))
-            duration = segment.duration
-            true_labels.extend([true_label] * int(duration))
-            pred_labels.extend([pred_label] * int(duration))
 
-    all_classes = set(true_labels+pred_labels)
-    y_true = np.array(true_labels)
-    y_pred = np.array(pred_labels)
-    macro_f1 = f1_score(y_true, y_pred, average="macro")
-    micro_f1 = f1_score(y_true, y_pred, average="micro")
-    result = {"macro_f1": macro_f1, "micro_f1": micro_f1}
-
-    return result
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--num_class", type=int, default=5)
+    return parser.parse_args()
 
 
-class_name_map = {
-    "Introduction_or_Opening": "Opening",
-    "Lecture_or_Presentation": "Lecture",
-    "Break_or_Transition": "Break",
-    "Conclusion_or_Summary": "Conclusion",
-    "Others": "Others",
-}
+def main():
+    args = parse_args()
+    num_class = args.num_class
+    class_name_map, class_dict, short_class_names = get_class_maps(num_class)
 
-if __name__ == "__main__":
+    wandb.init(project="Class stage classification", name=f"Whisper+ChatGPT {num_class}-class")
     test_files = [
         "(list_8)〔高二物化〕線上直播課程(00_01_29_40)",
         "(list_5)〔高一英文〕線上直播課程(00_00_27_45)",
@@ -88,16 +82,69 @@ if __name__ == "__main__":
     ]
     print("Calculating metrics for test files", test_files)
     label_dir = "data/label"
-    hyp_dir = "data/test"
+    hyp_dir = "data/pred"
     ref_list = []
     hyp_list = []
+
+    # parse hyp time to m
     for test_file in test_files:
         ref_df = pd.read_csv(f"{label_dir}/{test_file}.txt", sep="\s+", header=None)
         ref_df[2] = ref_df[2].map(lambda x: class_name_map[x])
         ref_list.append(ref_df)
         hyp_df = pd.read_csv(f"{hyp_dir}/{test_file}.txt", sep="\s+", header=None)
+        hyp_df.iloc[:, 0] = pd.to_timedelta(hyp_df.iloc[:, 0]).dt.total_seconds()
+        hyp_df.iloc[:, 1] = pd.to_timedelta(hyp_df.iloc[:, 1]).dt.total_seconds()
         hyp_df[2] = hyp_df[2].map(lambda x: class_name_map[x])
         hyp_list.append(hyp_df)
 
-    print(accuracy(ref_list, hyp_list))
-    print(f1_scores(ref_list, hyp_list))
+    true_labels = []
+    pred_labels = []
+    for reference_df, hypothesis_df in zip(ref_list, hyp_list):
+        hypothesis_df.iloc[0, 0] = 0
+        hypothesis_df.iloc[-1, 1] = reference_df.iloc[-1, 1]
+        for i in range(1, hypothesis_df.shape[0]):
+            if hypothesis_df.iloc[i, 0] != hypothesis_df.iloc[i - 1, 1]:
+                hypothesis_df.iloc[i - 1, 1] = hypothesis_df.iloc[i, 0]
+        start = 0
+        end = reference_df.iloc[-1, 1]
+        ref_duration = 0
+        hyp_duration = 0
+        reference = Annotation()
+        for i, row in reference_df.iterrows():
+            reference[Segment(row[0], row[1])] = row[2]
+            ref_duration += row[1] - row[0]
+        hypothesis = Annotation()
+        for i, row in hypothesis_df.iterrows():
+            hypothesis[Segment(row[0], row[1])] = row[2]
+            hyp_duration += row[1] - row[0]
+        error = IdentificationErrorAnalysis().difference(
+            reference, hypothesis, uem=Segment(start, end)
+        )
+
+        for segment, _ in error.itertracks():
+            label = error.get_labels(segment)
+            status, true_label, pred_label = next(iter(label))
+            duration = segment.duration
+            true_labels.extend([true_label] * int(duration))
+            pred_labels.extend([pred_label] * int(duration))
+
+    true_labels = [class_dict[label] for label in true_labels]
+    pred_labels = [class_dict[label] for label in pred_labels]
+    y_true = np.array(true_labels)
+    y_pred = np.array(pred_labels)
+    macro_f1 = f1_score(y_true, y_pred, average="macro")
+    micro_f1 = f1_score(y_true, y_pred, average="micro")
+    acc = (y_true == y_pred).sum().item() / len(y_true)
+    result = {"test_macro_f1": macro_f1, "test_micro_f1": micro_f1, "test_acc": acc}
+    wandb.log(result)
+    wandb.log(
+        {
+            "conf_mat": wandb.plot.confusion_matrix(
+                probs=None, y_true=y_true, preds=y_pred, class_names=short_class_names
+            )
+        }
+    )
+
+
+if __name__ == "__main__":
+    main()
